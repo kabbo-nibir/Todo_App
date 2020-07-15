d0ae95eecca29f48b017d851d8d7346a0bac569e
kabbo.nibir <kabbo.nibir@gmail.com>
2020-07-15 19:06:24 +0600

    primary ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screen/tasks_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false ,
+      home: TasksScreen(),
+    );
   }
 }
 
